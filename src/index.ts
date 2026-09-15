a903073d8358290e4cbbfeb1154d6a1fa4ae1733
kkpanfilov <kkpanfilov@outlook.com>
2026-09-15 19:06:29 +0300

    refactor: delete rebundant export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-export { noUnsafeRawSQL } from './rules/list/no-unsafe-raw-sql.js';
-
 export { convertToPrismaCall } from './ast/prisma/utils/convert-to-prisma-call.js';
 export { isPrismaCall } from './ast/prisma/utils/is-prisma-call.js';
 
