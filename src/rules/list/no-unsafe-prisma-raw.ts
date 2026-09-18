e55247a0aaaf9f9b12fc3ecc69b1ac3257bab7a9
kkpanfilov <kkpanfilov@outlook.com>
2026-09-18 17:38:31 +0300

    refactor(rule): make "no-unsafe-prisma-raw" rule to activates only on non-literal values

--- src/rules/list/no-unsafe-prisma-raw.ts
+++ src/rules/list/no-unsafe-prisma-raw.ts
@@ -2,5 +2,15 @@ import { PrismaCall } from '../../types/prisma.types.js';
 import { Rule } from '../../types/rule.eslint.types.js';
 
 export function noUnsafePrismaRaw(caller: PrismaCall, rule: Rule) {
-  return rule.methods.includes(caller.method);
+  const isRuleMethod = rule.methods.includes(caller.method);
+
+  if (!isRuleMethod) return false;
+
+  if (caller.args.length === 0) return false;
+
+  const firstArg = caller.args[0];
+
+  if (typeof firstArg === 'string') return false;
+
+  return true;
 }
